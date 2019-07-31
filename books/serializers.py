dd0f5e5557079f88a20f31b5a9821c26914df46b
kemoszn <mohdkamal47@gmail.com>
2019-08-01 00:15:33 +0200

    Removed duplicate code from serializer.py by using ModelSerializers

--- books/serializers.py
+++ books/serializers.py
@@ -1,24 +1,14 @@
 from rest_framework import serializers
 from .models import Book
 
-class BookSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=200)
-    author = serializers.CharField(max_length=100)
-    category = serializers.CharField(max_length=100)
-    price = serializers.DecimalField(max_digits=5, decimal_places=2)
-    in_stock = serializers.BooleanField(default=True)
-    quantity = serializers.DecimalField(max_digits=4, decimal_places=2)
-
-    def create(self, validated_data):
-        return Book.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.author = validated_data.get('author', instance.author)
-        instance.category = validated_data.get('category', instance.category)
-        instance.price = validated_data.get('price', instance.price)
-        instance.quantity = validated_data.get('quantity', instance.quantity)
-        instance.in_stock = validated_data.get('in_stock', instance.in_stock)
-        instance.save()
-        return instance 
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id',
+                    'title',
+                    'author',
+                    'category',
+                    'price',
+                    'in_stock',
+                    'quantity'
+        )
